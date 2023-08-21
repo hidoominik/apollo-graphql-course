c8641a8717d544b032648ffb8794e97301cebaa4
hidoominik <dominik.choma.41@gmail.com>
2023-08-21 13:09:50 +0200

    displaying fetched data

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -7,6 +7,8 @@ import { Layout } from "../components";
  */
 import { gql } from "../__generated__";
 import { useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import { QueryResult } from "../components";
 
 const TRACKS = gql(`
   query GetTracks {
@@ -27,11 +29,15 @@ const TRACKS = gql(`
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return "Loading...";
-
-  if (error) return `Error! ${error.message}`;
-
-  return <Layout grid>{JSON.stringify(data)}</Layout>;
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
